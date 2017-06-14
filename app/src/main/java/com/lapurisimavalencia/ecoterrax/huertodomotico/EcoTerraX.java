0f8ea49c1a4f8d93a5cb162f9ae83681599d3096
nestor <nesmaba@gmail.com>
2017-06-14 20:32:16 +0200

    Primer commit. Empezamos por la inerfaz gráfica.

---
+++ app/src/main/java/com/lapurisimavalencia/ecoterrax/huertodomotico/EcoTerraX.java
@@ -0,0 +1,13 @@
+package com.lapurisimavalencia.ecoterrax.huertodomotico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EcoTerraX extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eco_terra_x);
+    }
+}
